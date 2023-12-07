a1b710e53f4bc902c07346f22d4767880f668563
airton-sudo <73069455+airton-sudo@users.noreply.github.com>
2023-12-07 20:49:15 +0000

    fix: cadastro e listar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <h1>ola feito DB 2</h1>
       <a href="/aula/new/">cadastro </a><br />
-      <a href="#">listar</a>
+      <a href="/aula">listar</a>
     </div>
 
   )
