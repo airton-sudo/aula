83c937e5bd1a5ee0970090316cb0388009682f7a
airton-sudo <73069455+airton-sudo@users.noreply.github.com>
2023-12-07 20:40:16 +0000

    fix: DB com skip

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>ola feito DB</h1>
+    <div>
+      <h1>ola feito DB 2</h1>
+      <a href="#"></a>
+      <a href="#"></a>
+    </div>
+
   )
-  }
+}
+
