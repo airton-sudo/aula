17f7d66f59715923857c3424cb9bd358d933615d
airton-sudo <73069455+airton-sudo@users.noreply.github.com>
2023-12-10 15:03:26 +0000

    fix: listar e apagar

--- app/aula/page.tsx
+++ app/aula/page.tsx
@@ -1,5 +1,29 @@
-export default function listar(){
-    return(
-        <h1>apois</h1>
+import { sql } from "@vercel/postgres";
+
+export const revalidate =0
+
+export default async function ListCourse() {
+    const { rows } = await sql`SELECT * from aula`;
+    return (
+        <div>
+            <h1 className="text-center text-white">Lista de aulas</h1>
+
+            <table>
+                <thead>
+                    <tr> <td>Nome do professor</td> <td>Materia</td></tr>
+                </thead>
+                <tbody>
+                    {
+                        rows.map((aula) => {
+                            return (
+                                <tr key={aula.id}><td>{aula.name}</td> <td>{aula.materia}</td> </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+
+
+        </div>
     )
 }
