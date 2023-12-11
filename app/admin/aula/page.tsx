62755218c1b5cf215d6870eb9a432f5a95888b8d
airton-sudo <73069455+airton-sudo@users.noreply.github.com>
2023-12-11 21:47:36 +0000

    fix: excluir

--- app/admin/aula/page.tsx
+++ app/admin/aula/page.tsx
@@ -1,22 +1,37 @@
 import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
 
 export const revalidate =0
 
 export default async function ListAula() {
+    async function deleteAula(formData: FormData){
+        "use server"
+        const id = formData.get("id") as string;
+        await sql`DELETE from aula where id=${id}`
+        revalidatePath("/admin/aula")
+    }
     const { rows } = await sql`SELECT * from aula`;
     return (
         <div>
-            <h1 className="text-center text-white">Lista de aulas</h1>
+            <h1 className="text-center text-white">Lista de Cursos</h1>
 
             <table>
                 <thead>
-                    <tr> <td>Nome do professor</td> <td>Materia</td></tr>
+                    <tr> <td>Título do Curso</td> <td>Descrição</td></tr>
                 </thead>
                 <tbody>
                     {
                         rows.map((aula) => {
                             return (
-                                <tr key={aula.id}><td>{aula.name}</td> <td>{aula.materia}</td> </tr>
+                                <tr key={aula.id}><td>{aula.name}</td> <td>{aula.materia}</td> 
+                                <td>
+                                    <form >
+                                     <input type="text" hidden name="id" value={aula.id}/>   
+                                    <button formAction={deleteAula}>Excluir</button>
+                                    </form>
+                                
+                                </td> 
+                                </tr>
                             )
                         })
                     }
